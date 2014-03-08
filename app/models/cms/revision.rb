6b22550b69dee24160b6002a009272cfa23b3463
kliebes <shawn@Room19apps.com>
2014-03-08 16:23:59 -0600

    Added attar_accessible to Revision model

--- app/models/cms/revision.rb
+++ app/models/cms/revision.rb
@@ -2,6 +2,8 @@ class Cms::Revision < ActiveRecord::Base
   include Cms::Base
   
   serialize :data
+
+  attr_accessible :data
   
   # -- Relationships --------------------------------------------------------
   belongs_to :record, :polymorphic => true
